3a9e0d46f1e862a6f93e6400df9be6b133351b54
aphremthomas <aphremthomas@gmail.com>
2019-05-22 18:26:46 +0530

    colors added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var data=[10,20,30,40,70, 200,300];
 
 
-
+var scaleColor=d3.scale.linear()
+		.domain([0,1000])
+		.range(["red","green"]);
 
 var scaleBars = d3.scale.linear()
 		.domain([0,1000])
@@ -12,14 +14,16 @@ var canvas = d3.select("body")
 		.append("svg")
 		.attr("width",500)
 		.attr("height",500)
-		.style("background","red");
+		.style("background","black");
 var barChart = canvas.selectAll("rect")
 		.data(data)
 		.enter()
 			.append("rect")
 			.attr("height",function(d){return scaleBars(d)})
 			.attr("width", 10)
-			.attr("x",function(d,i){return i*20});
+			.attr("x",function(d,i){return i*20})
+			.attr("y",200)
+			.attr("fill",function(d){return scaleColor(d)});
 
 var line = canvas.append("line")
 		.attr("x1",0)
