ecd60f69e0cdbd6c335baf4f6fa60fa74f310487
aphremthomas <aphremthomas@gmail.com>
2019-05-22 14:25:01 +0530

    scales added

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-var data=[10,20,30,40,70];
+var data=[10,20,30,40,70, 200,300];
+
+
+
+
+var scaleBars = d3.scale.linear()
+		.domain([0,1000])
+		.range([0,500]);
+
 
 var canvas = d3.select("body")
 		.append("svg")
 		.attr("width",500)
-		.attr("height",500);
+		.attr("height",500)
+		.style("background","red");
+var barChart = canvas.selectAll("rect")
+		.data(data)
+		.enter()
+			.append("rect")
+			.attr("height",function(d){return scaleBars(d)})
+			.attr("width", 10)
+			.attr("x",function(d,i){return i*20});
+
+var line = canvas.append("line")
+		.attr("x1",0)
+		.attr("y1",0)
+		.attr("x2",20)
+		.attr("y2",20)
+		.attr("stroke","green")
+		.attr("stroke-width",10);
